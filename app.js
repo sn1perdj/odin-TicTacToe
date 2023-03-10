0e69849c05294b2695fa428d1a88672b08c182ed
sn1perdj <108739145+sn1perdj@users.noreply.github.com>
2023-03-10 15:01:31 +0530

    updated js, introduced module & IIFE

--- app.js
+++ app.js
@@ -1,117 +1,137 @@
-const board = [null, null, null, null, null, null, null, null, null];
+const TicTacToe = (function () {
+    // new game board
+    const board = new Array(9).fill(null);
+    const xSymbol = "./assets/x.png";
+    const oSymbol = "./assets/o.png";
+    let currentPlayer = "x";
+    let isGameOver = false;
 
-// Game Rules
-function checkWinner(board, symbol) {
-    // checking Horizonal
-    if (board[0] === symbol && board[1] === symbol && board[2] === symbol) {
-        return true;
-    } else if (
-        board[3] === symbol &&
-        board[4] === symbol &&
-        board[5] === symbol
-    ) {
-        return true;
-    } else if (
-        board[6] === symbol &&
-        board[7] === symbol &&
-        board[8] === symbol
-    ) {
-        return true;
-    }
+    //selecting all the sqaures from game board
+    const squares = document.querySelectorAll(".game_board > div");
+    const announcement = document.querySelector(".announcement");
+    const winnerText = document.querySelector(".announcement > p");
 
-    // checking Vertical
-    if (board[0] === symbol && board[3] === symbol && board[6] === symbol) {
-        return true;
-    } else if (
-        board[1] === symbol &&
-        board[4] === symbol &&
-        board[7] === symbol
-    ) {
-        return true;
-    } else if (
-        board[2] === symbol &&
-        board[6] === symbol &&
-        board[8] === symbol
-    ) {
-        return true;
-    }
+    // Main game function, initializer
+    const init = () => {
+        squares.forEach((square) => {
+            square.addEventListener("click", () => {
+                const squareIndex = Array.from(squares).indexOf(square);
+                if (
+                    !isGameOver &&
+                    square.textContent === "" &&
+                    board[squareIndex] === null
+                ) {
+                    board[squareIndex] = currentPlayer;
+                    const img = document.createElement("img");
+                    img.src = xSymbol;
+                    square.appendChild(img);
+                    winner();
+                    switchPlayer();
 
-    // checking diagonal
-    if (board[0] === symbol && board[4] === symbol && board[8] === symbol) {
-        return true;
-    } else if (
-        board[2] === symbol &&
-        board[4] === symbol &&
-        board[6] === symbol
-    ) {
-        return true;
-    }
-}
-
-// give random number
-function generateRandomEmptyIndex() {
-    const emptyIndices = board.reduce((acc, val, index) => {
-        if (val === null) {
-            acc.push(index);
-        }
-        return acc;
-    }, []);
-
-    if (emptyIndices.length === 0) {
-        return "There are no empty places in the board";
-    }
-
-    const randomIndex =
-        emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
-    return randomIndex;
-}
-
-//
-// All divs selector
-const announcement = document.querySelector(".announcement");
-const winner = document.querySelector(".winner");
-const board_block = document.querySelectorAll(".game_board > div");
-board_block.forEach((div) => {
-    div.addEventListener("click", () => {
-        if (div.innerHTML === "") {
-            // Player move
-            const img = document.createElement("img");
-            img.src = "./assets/x.png";
-            div.appendChild(img);
-            const index = Array.from(board_block).indexOf(div);
-            board[index] = "x";
-            const player_status = checkWinner(board, "x");
-            if (player_status === true) {
-                announcement.classList.add("visible");
-                winner.textContent = "Player has been won";
-            }
-
-            // Computer move
-            const comp_selection = generateRandomEmptyIndex();
-            if (typeof comp_selection === "string") {
-                winner.textContent = "TIE";
-            } else {
-                const comp_img = document.createElement("img");
-                comp_img.src = "./assets/o.png";
-                board_block[comp_selection].append(comp_img);
-                board[comp_selection] = "o";
-                const comp_status = checkWinner(board, "o");
-                // console.log(comp_status);
-                if (comp_status === true) {
-                    announcement.classList.add("visible");
-                    winner.textContent = "Computer has been won";
+                    if (!isGameOver) {
+                        computerMove();
+                    }
                 }
+            });
+        });
+    };
+
+    // Computer's Move
+    const computerMove = () => {
+        const randomIndex = randomIndex_Generator();
+        const displaySquare = Array.from(squares)[randomIndex];
+        board[randomIndex] = currentPlayer;
+        const img = document.createElement("img");
+        img.src = oSymbol;
+        displaySquare.appendChild(img);
+        winner();
+        switchPlayer();
+    };
+
+    // Generating a random number
+    const randomIndex_Generator = () => {
+        // generating a Array of empty places
+        const emptyIndices = board.reduce((acc, val, index) => {
+            if (val === null) {
+                acc.push(index);
+            }
+            return acc;
+        }, []);
+        // selecting a random place from the array which is empty
+        const randomIndex =
+            emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
+        return randomIndex;
+    };
+
+    // Checking for the Winner
+    const winner = () => {
+        const winningVariations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        // checking if any variations from above array matches.
+        for (let i = 0; i < winningVariations.length; i++) {
+            const [a, b, c] = winningVariations[i];
+            // checking
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                isGameOver = true;
+                announcement.classList.add("visible");
+                winnerText.textContent = `${currentPlayer} has been WON`;
             }
         }
-    });
+        // checking and declaring "TIE", if nobody managed to WIN
+        if (!board.includes(null) && !isGameOver) {
+            announcement.classList.add("visible");
+            winnerText.textContent = `It is a TIE`;
+        }
+    };
+
+    const switchPlayer = () => {
+        if (currentPlayer === "x") {
+            currentPlayer = "o";
+        } else {
+            currentPlayer = "x";
+        }
+    };
+
+    // Making Virtual board null again, and all squares go blank again
+    const reset = () => {
+        board.fill(null);
+        squares.forEach((square) => {
+            const img = square.querySelector("img");
+            if (img) {
+                img.remove();
+            }
+        });
+        currentPlayer = "x";
+        isGameOver = false;
+        announcement.classList.remove("visible");
+    };
+
+    return {
+        init,
+        reset,
+    };
+})();
+
+// Game Initializer
+TicTacToe.init();
+
+// onBoard Reset Button
+const resetBTN = document.querySelector(".resetBTN");
+resetBTN.addEventListener("click", () => {
+    TicTacToe.reset();
 });
 
-// button to reset the board and refresh the page
-function resetPage() {
-    window.location.reload();
-}
-
-const reset = document.querySelector(".resetBTN");
-const playAgain = document.querySelector(".announcement > a");
-reset.addEventListener("click", resetPage);
-playAgain.addEventListener("click", resetPage);
+// Popup reply button
+const replayBTN = document.querySelector(".announcement > a");
+replayBTN.addEventListener("click", () => {
+    TicTacToe.reset();
+});
